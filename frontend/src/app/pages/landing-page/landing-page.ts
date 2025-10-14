d43486cd2eeff343fa0bd863283a1c388f2a8ca5
andresveleze31 <andresvelezecheverry@gmail.com>
2025-10-14 10:41:19 -0500

    Feature: Full Landing Visual

--- frontend/src/app/pages/landing-page/landing-page.ts
+++ frontend/src/app/pages/landing-page/landing-page.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { Header } from '../../components/landing/header/header';
 import { Features } from '../../components/landing/features/features';
 import { Ceo } from '../../components/landing/ceo/ceo';
+import { PaymentsLanding } from '../../components/landing/payments-landing/payments-landing';
+import { Integration } from '../../components/landing/integration/integration';
+import { Banner } from '../../components/landing/banner/banner';
+import { Footer } from '../../components/landing/footer/footer';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Header, Features, Ceo],
+  imports: [Header, Features, Ceo, PaymentsLanding, Integration, Banner, Footer],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css'
 })
